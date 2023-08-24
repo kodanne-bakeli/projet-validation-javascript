2e358af9de805e7125da097a8010f5f62ea46070
Chams <tallbachir80@gmail.com>
2023-08-24 23:02:08 +0000

    recuperation de la branche firebase-config

--- cotisations.js
+++ cotisations.js
@@ -1,31 +1,31 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  sendPasswordResetEmail,
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    sendPasswordResetEmail,
 } from "firebase/auth";
 import {
-  getFirestore,
-  collection,
-  getDocs,
-  addDoc,
-  doc,
-  updateDoc,
-  deleteDoc,
+    getFirestore,
+    collection,
+    getDocs,
+    addDoc,
+    doc,
+    updateDoc,
+    deleteDoc,
 } from "firebase/firestore/lite";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAdYSscG955-wSnJz3eD4EAKYSw7dnnDb8",
-  authDomain: "validationjs-220cb.firebaseapp.com",
-  projectId: "validationjs-220cb",
-  storageBucket: "validationjs-220cb.appspot.com",
-  messagingSenderId: "968244401042",
-  appId: "1:968244401042:web:f6684d97e4168a23b0a74b",
+    apiKey: "AIzaSyAdYSscG955-wSnJz3eD4EAKYSw7dnnDb8",
+    authDomain: "validationjs-220cb.firebaseapp.com",
+    projectId: "validationjs-220cb",
+    storageBucket: "validationjs-220cb.appspot.com",
+    messagingSenderId: "968244401042",
+    appId: "1:968244401042:web:f6684d97e4168a23b0a74b",
 };
 
 // Initialize Firebase
@@ -34,5 +34,4 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 // Initialize Firebase firestore
 const db = getFirestore(app);
-
 
