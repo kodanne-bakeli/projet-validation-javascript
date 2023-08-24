3eb2cdcaba70712cb462fc24c367244602ecf1d7
Sam Laye <layesamss97@gmail.com>
2023-08-24 19:11:52 +0000

    Intégration finale

--- sidebarUser.js
+++ sidebarUser.js
@@ -11,3 +11,28 @@ menuElements.forEach(elem => {
 		elem.classList.add('style-nav-elem');
 	});
 });
+
+// ===========DROPDOWN PERSONNALISE=============
+let closeModalBtn = document.getElementById('closeModalBtn')
+let myModal = document.getElementById('myModal')
+let btnModal = document.getElementById('header-btn-down')
+
+btnModal.addEventListener('click', function(){
+	myModal.style.display = "block";
+})
+
+closeModalBtn.addEventListener('click', function(){
+	myModal.style.display = "none";
+})
+
+window.addEventListener('click', function(event){
+	let displayDropDown = document.getElementsByClassName('unNomDeClasseSpecial')[0]
+	if (
+		event.target.parentElement.parentElement === displayDropDown 
+		&& 
+		myModal.style.display === "block"
+	) {
+		myModal.style.display = "none"
+	}
+})
+// =============================================
