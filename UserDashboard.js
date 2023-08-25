c4e15ad944d62e1264f6e458f7edb2e1118112cd
Kodanne-bakeli <kodanne.bakeli@gmail.com>
2023-08-25 11:23:43 +0000

    merge

--- UserDashboard.js
+++ UserDashboard.js
@@ -10,7 +10,7 @@ for (let i= 1; i< dateOne.length; i++) {
     <tbody>
     <tr>
         <th scope="row">${dateOne[i]}</th>
-        <td>01/${i}/2023</td>
+        <td>01/0${i}/2023</td>
         <td>25.000FCFA</td>
         <td class="text-primary fw-bold"> En attente</td>
         <td><i class="bi bi-eye" data-bs-toggle="modal"
@@ -24,45 +24,94 @@ for (let i= 1; i< dateOne.length; i++) {
 `
 
 }
-for (let i= 1; i< dateTwo.length; i++) {
-    tableTwo.innerHTML += `<table>
-    
-    <tbody>
-    <tr>
-        <th scope="row">${dateTwo[i]}</th>
-        <td>01/${6+i}/2023</td>
-        <td>25.000FCFA</td>
-        <td class="text-primary fw-bold"> En attente</td>
-        <td><i class="bi bi-eye" data-bs-toggle="modal"
-                data-bs-target="#exampleModal1"></i>
-            <i class="bi bi-pencil-square"></i>
-        </td>
-    </tr>
-    
-</tbody>
-</table>
-`
 
-}
 function Next(){
+    tableTwo.innerHTML=`
+    <thead>
+ <tr>
+<th scope="col" class="bg-success text-primary">Mois</th>
+<th scope="col" class="bg-success text-primary">Date</th>
+<th scope="col" class="bg-success text-primary">Montant</th>
+<th scope="col" class="bg-success text-primary">Statut</th>
+<th scope="col" class="bg-success text-primary">Actions</th>
+ </tr>
+</thead>`
     document.querySelector(".active div").classList.remove("bg-success")
     document.querySelector(".active div").classList.add("text-info")
+    document.querySelector(".active div").classList.remove("text-white")
     document.querySelector(".active div").classList.add("bg-white")
-    document.querySelector(".active div").classList.add("d-none")
-    document.querySelector(".active div").classList.remove("active")
+    document.querySelector("#tableOne").classList.add("d-none")
     document.querySelector(".next div").classList.add("bg-success")
     document.querySelector(".next div").classList.add("active")
     document.querySelector(".next div").classList.add("text-white")
     document.querySelector(".next div").classList.remove("bg-white")
-    document.querySelector(".next div").classList.remove("d-none")
+    document.querySelector("#tableTwo").classList.remove("d-none")
+    document.querySelector(".prev").classList.remove("disabled")
+    document.querySelector(".prev").classList.add("fw-bold")
+    document.querySelector(".nexte").classList.add("disabled")
+    document.querySelector(".nexte").classList.remove("fw-bold")
+    for (let i= 1; i< dateTwo.length; i++) {
+        tableTwo.innerHTML += `<table>
+        
+        <tbody>
+        <tr>
+            <th scope="row">${dateTwo[i]}</th>
+            <td>01/${6+i}/2023</td>
+            <td>25.000FCFA</td>
+            <td class="text-primary fw-bold"> En attente</td>
+            <td><i class="bi bi-eye" data-bs-toggle="modal"
+                    data-bs-target="#exampleModal1"></i>
+                <i class="bi bi-pencil-square"></i>
+            </td>
+        </tr>
+        
+    </tbody>
+    </table>
+    `
+    
+    }
+    
 }
 function Prev(){
-    document.querySelector(".active div").classList.remove("bg-success")
-    document.querySelector(".active div").classList.remove("d-none")
-    document.querySelector(".active div").classList.add("text-info")
-    document.querySelector(".active div").classList.add("bg-white")
-    document.querySelector(".next div").classList.add("bg-success")
-    document.querySelector(".next div").classList.add("text-white")
-    document.querySelector(".next div").classList.add("d-none")
-    document.querySelector(".next div").classList.remove("bg-white")
+    tableOne.innerHTML=`
+    <thead>
+<tr>
+<th scope="col" class="bg-success text-primary">Mois</th>
+<th scope="col" class="bg-success text-primary">Date</th>
+<th scope="col" class="bg-success text-primary">Montant</th>
+<th scope="col" class="bg-success text-primary">Statut</th>
+<th scope="col" class="bg-success text-primary">Actions</th>
+</tr>
+</thead>`
+    document.querySelector(".active div").classList.add("bg-success")
+    document.querySelector(".active div").classList.add("text-white")
+    document.querySelector("#tableOne").classList.remove("d-none")
+    document.querySelector(".active div").classList.remove("text-info")
+    document.querySelector(".active div").classList.remove("bg-white")
+    document.querySelector(".next div").classList.remove("bg-success")
+    document.querySelector(".next div").classList.remove("text-white")
+    document.querySelector("#tableTwo").classList.add("d-none")
+    document.querySelector(".next div").classList.add("bg-white")
+    document.querySelector(".prev").classList.add("disabled")
+    document.querySelector(".nexte").classList.remove("disabled")
+    for (let i= 1; i< dateOne.length; i++) {
+        tableOne.innerHTML += `<table>
+        
+        <tbody>
+        <tr>
+            <th scope="row">${dateOne[i]}</th>
+            <td>01/${i}/2023</td>
+            <td>25.000FCFA</td>
+            <td class="text-primary fw-bold"> En attente</td>
+            <td><i class="bi bi-eye" data-bs-toggle="modal"
+                    data-bs-target="#exampleModal1"></i>
+                <i class="bi bi-pencil-square"></i>
+            </td>
+        </tr>
+        
+    </tbody>
+    </table>
+    `
+    
+    }
 }
