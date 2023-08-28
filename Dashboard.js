8e892f8c5726ad63c16b9941b7b402b3f157ea86
Oumar Ndongo <ohdjrdemette1203@gmail.com>
2023-08-28 01:28:54 +0000

    correction dashboard

--- Dashboard.js
+++ Dashboard.js
@@ -1,1 +1,19 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
 
+// import {
+//   getFirestore,
+//   collection,
+//   getDocs ,
+//   addDoc
+// } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAdYSscG955-wSnJz3eD4EAKYSw7dnnDb8",
+//     authDomain: "validationjs-220cb.firebaseapp.com",
+//     projectId: "validationjs-220cb",
+//     storageBucket: "validationjs-220cb.appspot.com",
+//     messagingSenderId: "968244401042",
+//     appId: "1:968244401042:web:f6684d97e4168a23b0a74b",
+// };
