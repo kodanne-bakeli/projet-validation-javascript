3be25a3b4249fc4377cd3e1e85b95716f47f2634
Kodanne-bakeli <kodanne.bakeli@gmail.com>
2023-08-31 20:29:34 +0000

    first commit

--- UserDashboard.js
+++ UserDashboard.js
@@ -211,8 +211,21 @@ btnprevs.forEach((btnprev)=>{
         console.log(names)
     }
 
+//-----------------SIDEBAR-------------//
+    let menuElements = document.querySelectorAll(".sidebar #navi #nav-ul li")
 
-
+    menuElements.forEach(elem => {
+        elem.addEventListener('click',() =>{
+            // Pour enlever le style
+            menuElements.forEach(autresElem =>{
+                autresElem.classList.remove('style-nav-elem');
+            });
+    
+            // Pour ajouter le style
+            elem.classList.add('style-nav-elem');
+        });
+    });
+    
 
 
 
