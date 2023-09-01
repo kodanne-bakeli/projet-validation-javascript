4351943adebc1e291ee48bb9c767af8af08e3623
Chams <tallbachir80@gmail.com>
2023-09-01 04:38:33 +0000

    retouchess

--- cotisations.js
+++ cotisations.js
@@ -16,16 +16,66 @@ fetch(URL, {
     .then((response) => response.json())
     .then((users) => {
         console.log(users);
+        let montantTotalCotiseEnMai = 0;
+        let montantTotalCotiseEnJuin = 0;
+        let montantTotalCaisse = 0;
+        // je stocke les totaux de chaque mois
+        let montantTotalCotiseParMois = {
+            0: 0,  //janvier
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0,
+            5: 0,
+            6: 0,
+            7: 0,
+            8: 0,
+            9: 0,
+            10: 0,
+            11: 0, //decembre
+        }
         users.forEach((user, index) => {
+            user.cotisations.forEach((cotisation) => {
+                const dateCotisation = new Date(cotisation.dateCotisation);
+                // console.log(dateCotisation);
+                const month = dateCotisation.getMonth();
+                // console.log(month);
+
+                // Accumuler le montant de cotisation pour le mois actuel
+                montantTotalCotiseParMois[month] += parseFloat(cotisation.montantCotisation);
+
+                if (month == 4) {
+                    montantTotalCotiseEnMai += parseFloat(cotisation.montantCotisation);
+                }
+                if (month == 5) {
+                    montantTotalCotiseEnJuin += parseFloat(cotisation.montantCotisation);
+                }
+
+            })
+
+            let cardMai = document.querySelector(".cardMai");
+            cardMai.innerHTML = ` <h6> Mai </h6>
+      <h5> ${montantTotalCotiseEnMai} FCFA</h5>
+      `
+            let cardJuin = document.querySelector(".cardJuin");
+            cardJuin.innerHTML = ` <h6> Juin </h6>
+      <h5> ${montantTotalCotiseEnJuin} FCFA</h5>
+      `
+            // Calculer la somme totale de toutes les cotisations pour tous les mois
+            for (const monthTotal of Object.values(montantTotalCotiseParMois)) {
+                montantTotalCaisse += monthTotal;
+            }
+            console.log(montantTotalCaisse);
             // Calcul de la somme des cotisations pour l'utilisateur actuel
             let montantTotalCotiseparUser = user.cotisations.reduce((total, cotisation) => {
                 total = total + parseFloat(cotisation.montantCotisation);
                 return (total);
             }, 0)
+
             let tr1 = document.createElement("tr");
             tr1.innerHTML = ` 
         <td>${users[index].prenom} ${users[index].name} </td>
-        <td>01/01/2023</td>
+        <td>${users[index].cotisations[index].dateCotisation} </td>
         <td> ${montantTotalCotiseparUser} FCFA </td>
         <td>${seuil - montantTotalCotiseparUser} FCFA </td>
         <td><button type="button" class="btn  btn-sm btn-confirmer my-0 fw-bold text-light" onclick="boutonConfirmer(this)">confirmer</button>
@@ -36,7 +86,18 @@ fetch(URL, {
             setTimeout(() => {
                 tr1.classList.add("fade-in-row-after");
             }, 600)
+            console.log(montantTotalCaisse);
         })
+        let cardTotalCaisse = document.querySelector(".cardTotalCaisse");
+        cardTotalCaisse.innerHTML = ` <h6> Total caisse </h6>
+        <h5> ${montantTotalCaisse} FCFA </h5>
+        <div class="progress pb-1 rounded-5 my-1" role="progressbar" aria-label="Basic example"
+                                    aria-valuenow="77" aria-valuemin="0" aria-valuemax="100" style="height:5px">
+                                    <div class="progress-bar  rounded-5"
+                                        style="width: 77%  ; background-color:#20df7f ;"></div>
+                                </div>
+                                <h6 class="text-end text-secondary">77% du seuil</h6>
+        `
     })
     .catch((error) => {
         console.log("erreur erreur ", error)
@@ -53,9 +114,4 @@ function boutonConfirmer(button) {
     let td = button.parentElement;
     td.appendChild(messageConfirmation);
 }
-
-
-
-
-
 
